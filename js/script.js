80d591bb807d25b2eb2c3f3ba6eb7aeb1e3e1fcb
Michael Saldarriaga <michael91sg@gmail.com>
2017-02-12 16:31:11 -0500

    Add two buttons, and add script file

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+$("#form").hide();
+$("#volver").hide();
+
+var elementos=document.querySelectorAll("h2");
+
+$(".opcion").toggle(function(e){
+  $("#volver").fadeIn(1000);
+  $("#form").fadeIn(1000);
+  $(".opcion").fadeOut(1000);
+  $(this).fadeIn(1000);
+  if(e.target==elementos[0]){
+  document.getElementById('label').innerHTML="Petición:";
+  }else if(e.target==elementos[1]){
+  document.getElementById('label').innerHTML="Queja:";
+}else if(e.target==elementos[2]){
+  document.getElementById('label').innerHTML="Reclamo:";
+}else if(e.target==elementos[3]){
+  document.getElementById('label').innerHTML="Sugerencia:";
+  }
+},
+function(){
+  $("#volver").fadeOut(1000);
+  $("#form").fadeOut(1000);
+  $(".opcion").fadeIn(1000);
+}
+);
+
+
+$("#volver").click(function(){
+  $("#volver").fadeOut(1000);
+  $("#form").fadeOut(1000);
+  $(".opcion").fadeIn(1000);
+  return false;
+});
+
+
+});
