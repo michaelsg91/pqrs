98f7c62a5f1b96e16fab1be1bc4005db2388ccbe
Michael Saldarriaga <michael91sg@gmail.com>
2017-02-13 14:08:54 -0500

    Delete unnecessary function, and add comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
+
+//Ocultando objetos iniciales
 $("#form").hide();
 $("#op").hide();
+//------------------------
 
-var elementos=document.querySelectorAll("h2");
+var elementos=document.querySelectorAll("h2");//Seleccionando todos los elementos h2
 
-$(".opcion").toggle(function(e){
+$(".opcion").toggle(function(e){ //funcion de accion de dos cliks
   $("#form").fadeIn(1000);
   $(".opcion").fadeOut(1000);
   $(this).fadeIn(1000);
   if(e.target==elementos[0]){
-  document.getElementById('label').innerHTML="Petición";
+  document.getElementById('label').innerHTML="Petición"; //Escribiendo en el objeto
   elementos[0].innerHTML="Peticiones - (Volver)";
   }else if(e.target==elementos[1]){
   document.getElementById('label').innerHTML="Queja";
@@ -19,16 +22,16 @@ $(".opcion").toggle(function(e){
   elementos[2].innerHTML="Reclamos - (Volver)";
 }else if(e.target==elementos[3]){
   document.getElementById('label').innerHTML="Sugerencia";
-  elementos[3].innerHTML="sugerencias - (Volver)";
+  elementos[3].innerHTML="Sugerencias - (Volver)";
   }
-  $("#op").val(document.getElementById('label').innerHTML);
+  $("#op").val(document.getElementById('label').innerHTML); //Guardando valor del label
 
 },
-function(e){
+function(e){ //Funcion de regreso
   elementos[0].innerHTML="Peticiones";
   elementos[1].innerHTML="Quejas";
   elementos[2].innerHTML="Reclamos";
-  elementos[3].innerHTML="sugerencias";
+  elementos[3].innerHTML="Sugerencias";
 
   $("#form").fadeOut(1000);
   $(".opcion").fadeIn(1000);
--- php/enviarMail.php
+++ php/enviarMail.php
@@ -1,9 +1,21 @@
 <?php
-include "js/script.js"
-
 if(isset($_POST["nombre"])){
   $destinatario="michael91sg@gmail.com";
-  $asunto="<script> document.write(opcion); </script>";
-  echo $asunto;
+  $asunto=$_POST["opcion"];
+  $remitente=$_POST["correo"];
+  $mensaje .="Tipo de documento: ". $_POST['documento'] . "\n";
+  $mensaje .="Número de documento: ". $_POST['numDoc'] . "\n";
+  $mensaje .="Nombre: ". $_POST['nombre'] . "\n";
+  $mensaje .="Apellido(s): ". $_POST['apellido'] . "\n\n\n";
+  $mensaje .="Comentario: \n". $_POST['comentarios'] . "\n";
+
+  $headers="From: " . $remitente . "\r\n" .
+            "Reply-To:" . $remitente . "\r\n" .
+            "X-Mailer: PHP/". phpversion();
+
+  mail($destinatario,$asunto,$mensaje,$headers); //funcion mail para enviar el correo
+
+  echo "La información se ha enviado con éxito";
+
 }
 ?>
