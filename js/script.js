83bbb1a8b638b1dd8766a1069d83b3dda893ed24
Michael Saldarriaga <michael91sg@gmail.com>
2017-02-13 13:42:16 -0500

    Delete button back, and replace its function in the h2

--- js/script.js
+++ js/script.js
@@ -1,40 +1,38 @@
 $(document).ready(function(){
 $("#form").hide();
-$("#volver").hide();
 $("#op").hide();
 
 var elementos=document.querySelectorAll("h2");
 
 $(".opcion").toggle(function(e){
-  $("#volver").fadeIn(1000);
   $("#form").fadeIn(1000);
   $(".opcion").fadeOut(1000);
   $(this).fadeIn(1000);
   if(e.target==elementos[0]){
   document.getElementById('label').innerHTML="Petición";
+  elementos[0].innerHTML="Peticiones - (Volver)";
   }else if(e.target==elementos[1]){
   document.getElementById('label').innerHTML="Queja";
+  elementos[1].innerHTML="Quejas - (Volver)";
 }else if(e.target==elementos[2]){
   document.getElementById('label').innerHTML="Reclamo";
+  elementos[2].innerHTML="Reclamos - (Volver)";
 }else if(e.target==elementos[3]){
   document.getElementById('label').innerHTML="Sugerencia";
+  elementos[3].innerHTML="sugerencias - (Volver)";
   }
   $("#op").val(document.getElementById('label').innerHTML);
 
 },
 function(e){
-  $("#volver").fadeOut(1000);
+  elementos[0].innerHTML="Peticiones";
+  elementos[1].innerHTML="Quejas";
+  elementos[2].innerHTML="Reclamos";
+  elementos[3].innerHTML="sugerencias";
+
   $("#form").fadeOut(1000);
   $(".opcion").fadeIn(1000);
 }
 );
-
-
-$("#volver").click(function(e){
-  $("#volver").fadeOut(1000);
-  $("#form").fadeOut(1000);
-  $(".opcion").fadeIn(1000);
-
-});
 
 });
